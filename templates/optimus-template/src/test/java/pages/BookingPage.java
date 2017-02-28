46c9e62b2b53517c52469754b6d1d9d46e116ee4
krishnb <kbandame@gmail.com>
2017-03-01 00:08:27 +0530

    Added optimus template code

---
+++ templates/optimus-template/src/test/java/pages/BookingPage.java
@@ -0,0 +1,16 @@
+package pages;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+
+public class BookingPage {
+    private AppiumDriver driver;
+
+    public BookingPage(AppiumDriver driver) {
+        this.driver = driver;
+    }
+
+    public void bookARide() {
+        //code to book a ride
+    }
+}
